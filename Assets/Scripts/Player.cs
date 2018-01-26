9a6b2041315a1b0c5b19396dfa28ad75fe81b14e
Qinye Li <qinyeli@umich.edu>
2018-01-25 23:04:01 -0800

    Item corn created & interacts properly with player
    * Deleted Cinemachine/Gizmos folder because it keeps generating a
    strange file
    
    Note:
    * For OnTriggerEnter2D to be called, both the object should have a
    collider && at least one of the objects should have rigidbody2D
    * For OnCollisionEnter2D to be called, both the object should have a
    collider && at least one of the object should have a non-kinematic
    rigidbody2D.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D),typeof(PlatformerMotor2D))]
+public class Player : MonoBehaviour {
+
+	public int countItem;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.layer == LayerMask.NameToLayer("Item")) {
+			countItem++;
+			Destroy(collider.gameObject);
+		}
+	}
+}
