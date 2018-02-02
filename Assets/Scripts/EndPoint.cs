3c7e6e91e62e2607707fa73611850370a59ea773
Qinye Li <qinyeli@umich.edu>
2018-02-02 00:57:46 -0800

    Append to previous commit; delted non-useful print

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EndPoint : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
-			print("lll");
 			AudioManager.Instance.PlayEndingMusic();
 		}
 	}
