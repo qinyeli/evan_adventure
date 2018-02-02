556ec8e6b806c156d3a190ae0164ca3b37150d39
Qinye Li <qinyeli@umich.edu>
2018-02-02 01:25:26 -0800

    Dying and respawning complete
    * Note that this approach of deadPoint and endPoint is not scalable, but
    it is fine for now since there is only one deadPoint and endPoint in
    this game

---
+++ Assets/Scripts/DeadPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadPoint : MonoBehaviour {
+
+	public GameObject rebornPoint;
+	GameObject player;
+	bool isSpawning = false;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (isSpawning) return;
+		if (collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
+			isSpawning = true;
+			player = collider.gameObject;
+			player.SetActive(false);
+			Invoke("SpawnPlayer", 1f);
+		}
+	}
+
+	void SpawnPlayer() {
+		player.SetActive(true);
+		player.transform.position = rebornPoint.transform.position;
+		isSpawning = false;
+	}
+}
